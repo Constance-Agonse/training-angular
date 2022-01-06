1445cee0eea4f19fba4ce2047db4931939efe0fb
Constance <constance@MacBook-Pro-de-Constance.local>
2022-01-06 10:33:14 +0100

    first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap;
+
+  ngOnInit(): void {
+      this.mySnap = new FaceSnap(
+      'Archibald',
+      'mon meilleur ami depuis tout petit',
+      new Date(),
+      6,
+      'https://img.huffingtonpost.com/asset/60fa6f884100000d7681bf15.jpeg?cache=CygCXsZoKQ&ops=scalefit_630_noupscale'
+      )
+  }
 }
